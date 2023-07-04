0112962967457aee7859ea16458ae0fa8c7b14fa
Alexander Morales <falexanmorales@gmail.com>
2023-07-04 07:37:18 -0500

    Se crea la clase "CrearCuenta" con el fin de entender que es una instacia

---
+++ src/ByteBank/CrearCuenta.java
@@ -0,0 +1,13 @@
+package ByteBank;
+
+public class CrearCuenta {
+    public static void main(String[] args) {
+        Cuenta primeraCuenta = new Cuenta();
+        primeraCuenta.saldo = 1000;
+        System.out.println("primeraCuenta = " + primeraCuenta.saldo);
+
+        Cuenta segundaCuenta = new Cuenta();
+        segundaCuenta.saldo = 500;
+        System.out.println("segundaCuenta = " + segundaCuenta.saldo);
+    }
+}
---
+++ src/ByteBank/Cuenta.java
@@ -0,0 +1,8 @@
+package ByteBank;
+public class Cuenta {
+        double saldo;
+        int agencia;
+        int numero;
+        String titular;
+
+}
