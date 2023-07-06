ca9dd615f39cd9fa41a0787a3a1f992a4ca31f03
Alexander Morales <falexanmorales@gmail.com>
2023-07-06 05:37:50 -0500

    se cambia codigo para explicar instancias

--- src/ByteBank/CrearCuenta.java
+++ src/ByteBank/CrearCuenta.java
@@ -2,12 +2,18 @@ package ByteBank;
 
 public class CrearCuenta {
     public static void main(String[] args) {
+
         Cuenta primeraCuenta = new Cuenta();
-        primeraCuenta.saldo = 1000;
+        primeraCuenta.saldo = 200;
+        System.out.println("primeraCuenta = " + primeraCuenta.saldo);
+        primeraCuenta.saldo +=100;
         System.out.println("primeraCuenta = " + primeraCuenta.saldo);
 
-        Cuenta segundaCuenta = new Cuenta();
-        segundaCuenta.saldo = 500;
-        System.out.println("segundaCuenta = " + segundaCuenta.saldo);
+        Cuenta segundaCuenta = primeraCuenta;
+        segundaCuenta.saldo = 50;
+        System.out.println("Primera cuenta tiene " + primeraCuenta.saldo);
+        System.out.println("Segunda cuenta tiene " + segundaCuenta.saldo);
+        System.out.println("primeraCuenta = " + primeraCuenta);
+        System.out.println("segundaCuenta = " + segundaCuenta);
     }
 }
